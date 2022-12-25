407eee9ac6252c9a0558ba618cbadf65863e4250
morita daiki <morita.daiki.works@gmail.com>
2022-12-25 23:59:53 +0900

    ➕ フォーム追加してuseRefで取得

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export function TodoForm() {
+  const [input_todo, setTodos] = useState();
+
+  const taskNameRef = useRef();
+
+  const AddTodo = () => {
+    const taskName = taskNameRef.current.value;
+    console.log(taskName);
+  }
+
   return (
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <div>
+        <input type="text" ref={taskNameRef}/>
+        <button onClick={AddTodo}>追加</button>
+    </div>
   )
 }
