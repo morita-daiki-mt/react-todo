c8981cbaeaea7d4cc5ac2e25c31d577aa9d592b7
morita daiki <morita.daiki.works@gmail.com>
2022-12-26 00:15:46 +0900

    ➕ 日付input追加、簡易空チェック

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
@@ -7,15 +5,24 @@ export function TodoForm() {
   const [input_todo, setTodos] = useState();
 
   const taskNameRef = useRef();
+  const taskDeadlineRef = useRef();
 
   const AddTodo = () => {
     const taskName = taskNameRef.current.value;
+    const taskDeadline = taskDeadlineRef.current.value;
+    if(taskName == '' || taskDeadline == ''){ return false; }
+
     console.log(taskName);
+    console.log(taskDeadline);
+    // setTodos((prevTodos) => {
+      // ここでTodosを作成
+    // })
   }
 
   return (
     <div>
         <input type="text" ref={taskNameRef}/>
+        <input type="date" ref={taskDeadlineRef}/>
         <button onClick={AddTodo}>追加</button>
     </div>
   )
