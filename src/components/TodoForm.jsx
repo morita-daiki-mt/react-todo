2d12d40eb184c9e23edd6ad05dc3befd100eacba
morita daiki <morita.daiki.works@gmail.com>
2023-01-10 01:29:38 +0900

    🚧 completeTodo作成中

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -6,6 +6,7 @@ export function TodoForm() {
   const [todos, setTodos] = useState([]);
   const [taskName, setTaskName] = useState('');
   const [taskDeadLine, setTaskDeadLine] = useState('');
+  const [taskComplete, setTaskComplete] = useState(false);
 
   const addTodo = useCallback(() => {
     if(taskName == '' || taskDeadLine == ''){ 
@@ -13,8 +14,12 @@ export function TodoForm() {
       return false; 
     }
 
-    setTodos((inputTodo) => ([...inputTodo, {task_name: taskName, task_deadline: taskDeadLine}]))
+    setTodos((inputTodo) => ([...inputTodo, {task_name: taskName, task_deadline: taskDeadLine, complete: false }]))
   }, [taskName, taskDeadLine])
+
+  const completeTodo = useCallback(() => {
+    todos.map((todo, index) => (index == 0 ? { complete: true} : todo))
+  }, [taskComplete])
 
   return (
     <div>
@@ -22,19 +27,37 @@ export function TodoForm() {
       <input type="date" value={taskDeadLine} onChange={(e) => setTaskDeadLine(e.target.value)}/>
       <button onClick={addTodo}>追加</button>
 
-      {
-          todos.map((todo, index) => {
+      <h1>未完了</h1>
+      {todos.map((todo, index) => {
+        if(!todo.complete){
           return(
             <div key={index}>
-              <h1>{todo.task_name}</h1>
+              <h3>{todo.task_name}</h3>
               <p>
                 {todo.task_deadline}
               </p>
-              <input type="checkbox" />
+              {todo.complete ? <p>完了</p> : <p>未完了</p>}
+              
+              <input type="checkbox" value={index} onChange={(e) => setTaskComplete(e.target.value)} onClick={completeTodo}/>
             </div>
           );
-        })
-      }
+        }
+      })}
+
+      <h1>完了済み</h1>
+      {todos.map((todo, index) => {
+        if(todo.complete){
+          return(
+            <div key={index}>
+              <h3>{todo.task_name}</h3>
+              <p>
+                {todo.task_deadline}
+              </p>
+              <input type="checkbox" value={index} onClick={completeTodo}/>
+            </div>
+          );
+        }
+      })}
     </div>
   )
 }
